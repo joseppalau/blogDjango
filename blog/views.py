b3f1eceb6a1bd6c1cfdbb64d1df0fe80e1e6dbf4
Administrador <administrador@iMacdeAistrador.home>
2018-12-10 19:26:52 +0100

    Post are created and published in front end

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    stuff_front_end = {'posts': posts}
+    return render(request, 'blog/post_list.html', stuff_front_end)
 
 # Create your views here.

7847867b4860b0902c3f8ca85c889678772964e0
Administrador <administrador@iMacdeAistrador.home>
2018-12-11 09:28:53 +0100

    enlaces a post_detail completados

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    stuff_front_end = {'posts': posts}
-    return render(request, 'blog/post_list.html', stuff_front_end)
+    stuff_frontend = {'posts': posts}
+    return render(request, 'blog/post_list.html', stuff_frontend)
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    stuff_frontend = {'post':post}
+    return render(request, 'blog/post_detail.html', stuff_frontend)
 # Create your views here.

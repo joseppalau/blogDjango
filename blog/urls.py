31a5faf88885b45e3a142af1024738be5dc5738d
Administrador <administrador@iMacdeAistrador.home>
2018-12-12 21:53:34 +0100

    comit 2

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.edit_post, name="edit_post")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 
 
 def post_list(request):
@@ -8,8 +9,27 @@ def post_list(request):
     stuff_frontend = {'posts': posts}
     return render(request, 'blog/post_list.html', stuff_frontend)
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     stuff_frontend = {'post':post}
     return render(request, 'blog/post_detail.html', stuff_frontend)
 # Create your views here.
+
+
+def edit_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        stuff_frontend = {'form': form}
+    return render(request, 'blog/post_edit.html', stuff_frontend)
+
+
+
